98a0b18006e7a1cefdb7aad8c6c22802512822b9
cagri <cyucel@gmail.com>
2024-09-22 19:43:08 +0100

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.Maui.Audio;
+
+namespace AudioTest;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IAudioPlayer _audioPlayer;
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+    public static async void PlayAudio(string fileName)
+    {
+        var audioPlayer =
+            AudioManager.Current.CreatePlayer(await FileSystem.OpenAppPackageFileAsync($"Sounds/{fileName}"),
+                new AudioPlayerOptions
+                {
+#if IOS || MACCATALYST
+                    CategoryOptions = AVFoundation.AVAudioSessionCategoryOptions.MixWithOthers
+#endif
+                });
+        audioPlayer.Play();
+    }
+    
+}
